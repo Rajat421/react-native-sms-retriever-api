5bc2e13c342ca877387cae995dd5766052742782
Gaurav Ahuja <gaurav@mpaani.com>
2020-06-26 22:11:27 +0530

    Modify annotation module import for the AndroidX support

--- android/src/main/java/com/reactlibrary/RNSmsRetrieverBroadcastReciever.java
+++ android/src/main/java/com/reactlibrary/RNSmsRetrieverBroadcastReciever.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
